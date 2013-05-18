4034ea4d604ceb64787d6c8664ac136b8edec966
Chris Ferdinandi <hello@gomakethings.com>
2013-05-17 21:38:22 -0400

    Close dropdown menus when user clicks outside of them

--- basic/drop.js
+++ basic/drop.js
@@ -6,11 +6,25 @@
  * ============================================================= */
 
 $(function () {
+    // Close dropdown menus when you click outside of them    
+    $(document).click(function(){
+      $('.dropdown > a').removeClass('active'); // Remove any '.active' classes from dropdown links
+      $('.dropdown').removeClass('active'); // Remove any '.active' classes from dropdown list items
+      $('.dropdown-menu').removeClass('active'); // Hide any visible dropdown menus
+    });
+
+    // When a dropdown menu link is clicked
     $('.dropdown > a').click(function(e) {
-        e.preventDefault();
-        $(this).toggleClass('active').next($('.dropdown-menu')).toggleClass('active');
-        $(this).parent().siblings('.dropdown').removeClass('active').children('a').removeClass('active').next($('.dropdown-menu')).removeClass('active');
-        $(this).parent('.dropdown').toggleClass('active');
+        e.stopPropagation(); // Stop the "close all dropdowns" function
+        e.preventDefault(); // Prevent the default link action
+        $(this).toggleClass('active').next($('.dropdown-menu')).toggleClass('active'); // If the dropdown menu is hidden, show it. Otherwise, hide it.
+        $(this).parent('.dropdown').toggleClass('active'); // Add/remove '.active' class to the dropdown list item
+        $(this).parent().siblings('.dropdown').removeClass('active').children('a').removeClass('active').next($('.dropdown-menu')).removeClass('active'); // Hide all other dropdown menus
+    });
+
+    // When click inside a dropdown menu
+    $('.dropdown-menu').click(function(e) {
+        e.stopPropagation(); // Stop the "close all dropdowns" function
     });
 });
 
