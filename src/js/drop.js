847df679010f008c6ff3bb2fd8b63758e2d5f87a
Chris Ferdinandi <hello@gomakethings.com>
2015-03-08 22:58:19 -0400

    Updated UMD wrapper

--- src/js/drop.js
+++ src/js/drop.js
@@ -1,12 +1,12 @@
 (function (root, factory) {
 	if ( typeof define === 'function' && define.amd ) {
-		define('drop', factory(root));
+		define([], factory);
 	} else if ( typeof exports === 'object' ) {
-		module.exports = factory(root);
+		module.exports = factory;
 	} else {
 		root.drop = factory(root);
 	}
-})(this, function (root) {
+})(this, function (window) {
 
 	'use strict';
 
@@ -15,7 +15,7 @@
 	//
 
 	var drop = {}; // Object for public APIs
-	var supports = !!document.querySelector && !!root.addEventListener; // Feature test
+	var supports = !!document.querySelector && !!window.addEventListener; // Feature test
 	var settings;
 
 	// Default settings
